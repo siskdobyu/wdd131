d5f94b40e2fc3f81774d3237fb147e7a12efd924
siskdobyu <karinadolojan@gmail.com>
2025-06-01 09:00:51 +0800

    with changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -10,10 +10,10 @@ year.innerHTML = `<span>${today.getFullYear()}</span>`;
 const lastModified = document.querySelector("#lastmodified");
 
 lastModified.innerHTML = `<span class="highlight">Last modification: ${document.lastModified}</span>`;
-});
 
+// Hamburger Toggle
 const hamburgerBtn = document.getElementById("hamburgerBtn");
-const nav = document.getElementById("primaryNav");
+const nav = document.getElementById("mobileNav");
 
 hamburgerBtn.addEventListener("click", () => {
     nav.classList.toggle("open");
@@ -21,4 +21,6 @@ hamburgerBtn.addEventListener("click", () => {
     // Change symbol from ☰ to ✖ and back
     const isOpen = nav.classList.contains("open");
     hamburgerBtn.innerHTML = isOpen ? "&#10005;" : "&#9776;"; // ✖ or ☰
-});    
+    });  
+
+});
