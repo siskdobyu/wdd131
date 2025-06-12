b35b95008ba548c48b2fbb9656d40fa5b7b70ad3
siskdobyu <karinadolojan@gmail.com>
2025-06-12 20:21:42 +0800

    with updates in styles

--- scripts/place.js
+++ scripts/place.js
@@ -1,1 +1,12 @@
-document.getElementById("lastModified").textContent = document.lastModified;
+
+const year = document.querySelector("#year");
+
+// use the date object
+const today = new Date();
+
+
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+const lastModified = document.querySelector("#lastmodified");
+
+lastModified.innerHTML = `<span class="highlight">Last modification: ${document.lastModified}</span>`;
