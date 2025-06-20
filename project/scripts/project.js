7a5f5324f4df2b2dfe590c84de866c5aa4454cad
siskdobyu <karinadolojan@gmail.com>
2025-06-21 05:58:14 +0800

    with updates

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -9,3 +9,11 @@ year.innerHTML = `<span>${today.getFullYear()}</span>`;
 const lastModified = document.querySelector("#lastmodified");
 
 lastModified.innerHTML = `<span class="highlight">Last modification: ${document.lastModified}</span>`;
+
+// responsive views
+const menuBtn = document.getElementById('hamburgerBtn');
+const nav = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('open');
+});
