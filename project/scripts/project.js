891a9401919d3f526185c898dba304d90506491d
siskdobyu <karinadolojan@gmail.com>
2025-06-21 04:43:30 +0800

    with updates

---
+++ project/scripts/project.js
@@ -0,0 +1,11 @@
+const year = document.querySelector("#year");
+
+// use the date object
+const today = new Date();
+
+
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+const lastModified = document.querySelector("#lastmodified");
+
+lastModified.innerHTML = `<span class="highlight">Last modification: ${document.lastModified}</span>`;
